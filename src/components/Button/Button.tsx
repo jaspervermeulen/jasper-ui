b4a475e32b57dac689717e11dd0e3b5d01703f7d
jaspervermeulen <jaspervermeulen@icloud.com>
2022-11-26 19:22:34 +0100

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+interface ButtonProps {
+  label: string;
+}
+
+export default function Button(props: ButtonProps){
+  return(
+    <button>{props.label}</button>
+  )
+}
